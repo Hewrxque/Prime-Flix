af3505fee0fb9eeed5d1e24c0b9a52e7f82837dd
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-07-02 22:07:29 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Filme from "./Pages/Filme";
+import Header from "./Components/Header";
 
 export default function RoutesApp() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Filme" element={<Filme />} />
+        <Route path="/Filme/:id" element={<Filme />} />
       </Routes>
     </BrowserRouter>
   );
