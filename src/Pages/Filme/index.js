a5bbe6ac68d039391f3f4a9d2b465c2117c03a8d
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-07-07 21:39:50 -0300

    style update

--- src/Pages/Filme/index.js
+++ src/Pages/Filme/index.js
@@ -50,6 +50,15 @@ export default function Filme() {
       <h3>Sinopse</h3>
       <span>{filme.overview}</span>
       <strong>Avaliaçao: {filme.vote_average} /10</strong>
+
+      <div className="area-buttons">
+        <button>
+          Salvar
+        </button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
     </div>
   );
 }
