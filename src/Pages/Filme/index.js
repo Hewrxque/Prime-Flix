292378f25a3ac26e1dadf6a20a7be168a0a537ea
Hewrxque <josehenriquevieiradacruz@gmail.com>
2023-07-11 14:42:45 -0300

    update

--- src/Pages/Filme/index.js
+++ src/Pages/Filme/index.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import api from "../../Services/api";
-import "./filme.css"
+import "./filme.css";
+
 export default function Filme() {
   const { id } = useParams();
-  const [filme, setFilme] = useState(true)
-  const [loading, setLoading] = useState({})
+  const navigate = useNavigate();
+
+  const [filme, setFilme] = useState(true);
+  const [loading, setLoading] = useState({});
+
   useEffect(() => {
     async function loadFilme() {
-      await api.get(
-        `https://api.themoviedb.org/3/movie/ ${id}`,
-        {
+      await api
+        .get(`https://api.themoviedb.org/3/movie/ ${id}`, {
           params: {
             api_key: "540e44f53147417276c62d0661e2f51f",
             language: "pt-BR",
@@ -21,42 +24,45 @@ export default function Filme() {
           setLoading(false);
         })
         .catch(() => {
-          console.log("Filme nao encontrado")
-        })
+          console.log("Filme nao encontrado");
+          navigate("/", { replace: true });
+          return;
+        });
     }
     loadFilme();
 
-    return() => {
-      console.log("Componente desmontado")
-    }
-  }, []);
+    return () => {
+      console.log("Componente desmontado");
+    };
+  }, [navigate, id]);
 
-
-  if(loading){
-    return(
+  if (loading) {
+    return (
       <div>
-        <h1 className="filme-info">
-          Carregando Detalhes...
-        </h1>
+        <h1 className="filme-info">Carregando Detalhes...</h1>
       </div>
-    )
+    );
   }
   return (
     <div className="filme-info">
-      <h1>
-      {filme.title}
-      </h1>
-      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
       <h3>Sinopse</h3>
       <span>{filme.overview}</span>
       <strong>Avaliaçao: {filme.vote_average} /10</strong>
 
       <div className="area-buttons">
+        <button>Salvar</button>
         <button>
-          Salvar
-        </button>
-        <button>
-          <a href="#">Trailer</a>
+          <a
+            target="blank"
+            href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
+          >
+            Trailer
+          </a>
         </button>
       </div>
     </div>
